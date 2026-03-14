fd01fb6dba642a9039f6c27745119c37e399eae7
zamechatelnyipredel <zamechatelnyipredel@gmail.com>
2026-03-14 16:38:46 +0300

    Add files via upload

---
+++ lab4/blog/articles/views.py
@@ -0,0 +1,15 @@
+from django.http import Http404
+from django.shortcuts import render
+
+from articles.models import Article
+
+
+def archive(request):
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
+
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, "article.html", {"post": post})
+    except Article.DoesNotExist:
+        raise Http404
---
+++ lab4/blog/blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from articles import views
+
+urlpatterns = [
+    path('', views.archive, name='archive'),
+    path('admin/', admin.site.urls),
+    path('article/<int:article_id>/', views.get_article, name='get_article'),
+]
