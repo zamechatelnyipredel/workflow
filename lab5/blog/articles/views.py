1b693e385211fa40bfc2e5a98e10cdced10145f0
zamechatelnyipredel <zamechatelnyipredel@gmail.com>
2026-03-14 16:39:58 +0300

    Add files via upload

---
+++ lab5/blog/articles/views.py
@@ -0,0 +1,46 @@
+from django.http import Http404
+from django.shortcuts import render, redirect
+
+from articles.models import Article
+
+
+def archive(request):
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
+
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, "article.html", {"post": post})
+    except Article.DoesNotExist:
+        raise Http404
+
+
+def create_post(request):
+    if not request.user.is_anonymous:
+        if request.method == "POST":
+            # обработать данные формы, если метод POST
+            form = {
+                'text': request.POST["text"],
+                'title': request.POST["title"]
+            }
+            # Проверка на уникальность названия
+            if Article.objects.filter(title=form['title']).exists():
+                form['errors'] = "Статья с таким названием уже существует"
+                return render(request, 'create_post.html', {'form': form})
+            # в словаре form будет храниться информация, введенная пользователем
+            if form["text"] and form["title"]:
+                # если поля заполнены без ошибок
+                article = Article.objects.create(text=form["text"],
+                                                 title=form["title"],
+                                                 author=request.user)
+                return redirect('get_article', article_id=article.id)
+            # перейти на страницу поста
+            else:
+                # если введенные данные некорректны
+                form['errors'] = "Не все поля заполнены"
+                return render(request, 'create_post.html', {'form': form})
+        else:
+            # просто вернуть страницу с формой, если метод GET
+            return render(request, 'create_post.html', {})
+    else:
+        raise Http404
